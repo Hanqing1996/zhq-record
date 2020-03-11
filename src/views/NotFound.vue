90ba2da3964d699935b85a15ce9303ea6579b584
zhq <zhq199622@163.com>
2020-03-11 10:27:36 +0800

    添加 404 页面

---
+++ src/views/NotFound.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <div>404</div>
+        <router-link to="/money">返回首页</router-link>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "NotFound"
+    }
+</script>
+
+<style scoped>
+
+</style>
