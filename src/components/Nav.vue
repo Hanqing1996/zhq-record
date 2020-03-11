6d6fa1bce78e0d6e1d3c28d8ecbb30e28dc6d648
zhq <zhq199622@163.com>
2020-03-11 13:14:43 +0800

    添加导航栏样式

--- src/components/Nav.vue
+++ src/components/Nav.vue
@@ -1,17 +1,15 @@
 <template>
-    <div class="nav">
-        <router-link to="/money">
+    <nav class="nav">
+        <router-link to="/money" class="nav-item" :class="selected" @onclick="">
             <Icon name="money"></Icon>
         </router-link>
-        |
-        <router-link to="/labels">
+        <router-link to="/labels" class="nav-item">
             <Icon name="labels"></Icon>
         </router-link>
-        |
-        <router-link to="/statistics">
+        <router-link to="/statistics" class="nav-item">
             <Icon name="statistics"></Icon>
         </router-link>
-    </div>
+    </nav>
 </template>
 
 <script lang="ts">
@@ -20,8 +18,22 @@
     }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
     .nav {
-
+        display: flex;
+        box-shadow: 0 0 3px rgba(0, 0, 0, 0.25);
+        flex-direction: row;
+        font-size: 12px;
+        > .nav-item {
+            padding: 2px 0;
+            width: 33.33333%;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            flex-direction: column;
+        }
+        > .nav-item.selected {
+            color: red;
+        }
     }
 </style>
