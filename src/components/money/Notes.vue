fd416b914bfd74c524d6cd70a6a7e343f27505fb
zhq <zhq199622@163.com>
2020-03-12 10:08:11 +0800

    money 页面模块化

---
+++ src/components/money/Notes.vue
@@ -0,0 +1,33 @@
+<template>
+    <label class="notes">
+        <span class="name">备注</span>
+        <input type="text" placeholder="在这里输入备注">
+    </label>
+</template>
+
+<script>
+    export default {
+        name: "Notes"
+    }
+</script>
+
+<style lang="scss" scoped>
+
+    /*notes*/
+    .notes {
+        font-size: 14px;
+        background: #f5f5f5;
+        padding-left: 16px;
+        display: flex;
+        align-items: center;
+        .name {
+            padding-right: 16px;
+        }
+        input {
+            height: 64px;
+            background: transparent;
+            border: none;
+            padding-right: 16px;
+        }
+    }
+</style>
---
+++ src/components/money/Types.vue
@@ -0,0 +1,41 @@
+<template>
+    <ul class="types">
+        <li class="selected">收入</li>
+        <li>支出</li>
+    </ul>
+</template>
+
+<script>
+    export default {
+        name: "Types"
+    }
+</script>
+
+<style lang="scss" scoped>
+    /*types*/
+    .types{
+        display: flex;
+        flex-direction: row;
+        background-color: #c4c4c4;
+        font-size: 24px;
+        > li{
+            width: 50%;
+            height: 64px;
+            display: flex;
+            justify-content: center;
+            align-items: center;
+            position: relative;
+            text-align: center;
+            &.selected::after{
+                content: '';
+                position: absolute;
+                bottom: 0;
+                left: 0;
+                width: 100%;
+                height: 4px;
+                background: #333;
+            }
+        }
+    }
+
+</style>
