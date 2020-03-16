5a5e581ad6f47bff3b4a44103ff9107f0973afc0
zhq <zhq199622@163.com>
2020-03-16 17:59:31 +0800

    删除 recordStore，tagStore

--- src/custom.d.ts
+++ src/custom.d.ts
@@ -12,18 +12,3 @@ type Tag={id:number,name:string}
 
 type TagList=Tag[]
 type RecordList=RecordItem[]
-
-// interface RecordStore{
-//     recordList:RecordList,
-//     cloneRecord:(record: RecordList | RecordItem)=>RecordList|RecordItem,
-//     save:()=>void,
-//     addRecord:(record:RecordItem)=>void
-// }
-
-interface TagStore{
-    tagList:TagList,
-    addTag:(name: string) => 'duplicated' | 'success',
-    updateTag:(id: number, name: string) => 'not found' | 'duplicated' | 'success',
-    removeTag:(id: number) => 'not found' | 'success',
-    findTag:(id:number)=>Tag
-}
