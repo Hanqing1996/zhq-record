0c214301e8a61229e9220ac1d681f351a79c719a
zhq <zhq199622@163.com>
2020-03-24 16:53:41 +0800

    添加手机浏览二维码

--- src/main.ts
+++ src/main.ts
@@ -26,3 +26,14 @@ window.onload=function () {
     window.scrollTo(0,10000)
   },0)
 }
+
+if(document.documentElement.clientWidth>500){
+  window.alert('请使用手机打开本页面，以保证浏览效果')
+  const img=document.createElement('img')
+  img.src='/qrcode.png'
+  img.style.position='fixed'
+  img.style.top='50%'
+  img.style.left='50%'
+  img.style. transform='translate(-50%,-50%)';
+  document.body.appendChild(img)
+}
