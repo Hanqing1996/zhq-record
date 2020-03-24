2ed975df6685b824c349e8ac9181cde410a43650
zhq <zhq199622@163.com>
2020-03-24 16:54:08 +0800

    deploy

---
+++ precache-manifest.ee8134199cdb57933520064627b007b4.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0836a339c4139ba0f2ea",
+    "url": "/zhq-record/css/app.99a8fe99.css"
+  },
+  {
+    "revision": "098bb86b81c5ea6225481ced6e8a432a",
+    "url": "/zhq-record/index.html"
+  },
+  {
+    "revision": "0836a339c4139ba0f2ea",
+    "url": "/zhq-record/js/app.b89d0da0.js"
+  },
+  {
+    "revision": "54ef730756101e819072",
+    "url": "/zhq-record/js/chunk-vendors.4ac06b7e.js"
+  },
+  {
+    "revision": "0a031efe9afd90657d068d84d1487b37",
+    "url": "/zhq-record/manifest.json"
+  },
+  {
+    "revision": "93d7d675d47586adfbdc3440520289de",
+    "url": "/zhq-record/qrcode.png"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/zhq-record/robots.txt"
+  }
+]);
